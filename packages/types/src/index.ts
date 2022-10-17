f4941ebf4aed9c0d0fefd210a3aaf922dcf21704
白云涛 <yuntaoBai@163.com>
2022-10-17 13:46:09 +0800

    add connector

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -1,13 +1,20 @@
+/***
+ * AbstractConnectorArguments
+ ***/
 export interface AbstractConnectorArguments {
   supportedChainIds?: number[]
 }
-
+/***
+ * ConnectorUpdate
+ ***/
 export interface ConnectorUpdate<T = number | string> {
   provider?: any
   chainId?: T
   account?: null | string
 }
-
+/***
+ * ConnectorEvent
+ ***/
 export enum ConnectorEvent {
   Update = 'Web3VueUpdate',
   Error = 'Web3VueError',
