d3ff4a02281b2651617b0ff10dc8552aa43bb101
白云涛 <yuntaoBai@163.com>
2022-10-19 10:03:19 +0800

    build

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,13 +1,13 @@
 import Web3 from 'web3'
 
 export default class VueWeb3 extends Web3 {
-    static install: (_Vue, options?: { provider: null }) => void;
+    static install: (options?: { provider: null }) => void;
     static useWallet: () => void;
     constructor() {
         super();
     }
 }
 
-VueWeb3.install = (_Vue, options = { provider: null })=> {
+VueWeb3.install = ()=> {
 
 }
