2d6afa025abf80bc8a5de3a4027786a1d76fdd67
白云涛 <yuntaoBai@163.com>
2022-10-19 15:13:02 +0800

    edit core

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -1,25 +1,4 @@
-// import { AbstractConnector } from '@web3-vue3/abstract-connector';
 import { ConnectorUpdate } from "@web3-vue3/types";
-// export interface Web3ReactManagerFunctions {
-//     activate: (connector: AbstractConnector, onError?: (error: Error) => void, throwErrors?: boolean) => Promise<void>;
-//     setError: (error: Error) => void;
-//     deactivate: () => void;
-// }
-// export interface Web3VueManagerReturn extends Web3ReactManagerFunctions {
-//     connector?: AbstractConnector;
-//     provider?: any;
-//     chainId?: number;
-//     account?: null | string;
-//     error?: Error;
-// }
-// export interface Web3VueContextInterface<T = any> extends Web3ReactManagerFunctions {
-//     connector?: AbstractConnector;
-//     library?: T;
-//     chainId?: number;
-//     account?: null | string;
-//     active: boolean;
-//     error?: Error;
-// }
 
 export type connector = {
     activate(): Promise<ConnectorUpdate>
