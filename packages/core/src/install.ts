027f3f0c41679840e79f07088ec9f5ca7c38cf39
白云涛 <yuntaoBai@163.com>
2022-10-19 15:38:22 +0800

    edit core

--- packages/core/src/install.ts
+++ packages/core/src/install.ts
@@ -2,10 +2,10 @@ import Web3 from 'web3'
 import { provider } from 'web3-core'
 import store from './store'
 
-export default function (vue:any, options: {provider: provider}) {
+export default function (app:any, options: {provider: provider}) {
     const { ethereum } = <any>window
     const web3 = new Web3(options.provider || ethereum)
     store.web3 = web3
-    vue.config.globalProperties.$web3Utils = web3.utils
+    app.config.globalProperties.$web3Utils = web3.utils
     console.log(options)
 }
