9d24779b4888609f23d60a3df682c5a97c26a0aa
白云涛 <yuntaoBai@163.com>
2022-10-19 11:34:11 +0800

    edit core

--- packages/core/src/install.ts
+++ packages/core/src/install.ts
@@ -1,5 +1,7 @@
 import store from "sore";
 
-export default function (app, options) {
+export default function (app: object, options?: object) {
+    // @ts-ignore
     app.config.globalProperties.$web3Utils = store.web3.utils
+    console.log(options)
 }
