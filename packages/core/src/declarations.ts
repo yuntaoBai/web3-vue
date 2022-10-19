6d7ba32e00a16d9ba1ef0d9634998f1c02851ea0
白云涛 <yuntaoBai@163.com>
2022-10-19 17:38:49 +0800

    edit core

--- packages/core/src/declarations.ts
+++ packages/core/src/declarations.ts
@@ -1,1 +1,12 @@
+interface Ethereum {
+    send: unknown
+    enable: () => Promise<string[]>
+    on?: (method: string, listener: (...args: any[]) => void) => void
+    removeListener?: (method: string, listener: (...args: any[]) => void) => void
+}
+
+declare interface Window {
+    ethereum?: Ethereum
+}
+
 declare module 'vue'
--- packages/core/src/install.ts
+++ packages/core/src/install.ts
@@ -2,9 +2,9 @@ import Web3 from 'web3'
 import { provider } from 'web3-core'
 import store from './store'
 
-export default function (app:any, options: {provider: provider}) {
-    const { ethereum } = <any>window
-    const web3 = new Web3(options.provider || ethereum)
+export default function (app:any, options?: { provider: provider }) {
+    const providerOptions = options && options.provider ? options.provider : window.ethereum
+    const web3 = new Web3(<any>providerOptions)
     store.web3 = web3
     app.config.globalProperties.$web3Utils = web3.utils
     console.log(options)
