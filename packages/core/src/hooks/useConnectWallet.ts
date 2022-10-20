13617a66850414d2dd407366896076bfdca86844
白云涛 <yuntaoBai@163.com>
2022-10-20 23:23:23 +0800

    compatible vue2

--- packages/core/src/hooks/useConnectWallet.ts
+++ packages/core/src/hooks/useConnectWallet.ts
@@ -3,21 +3,25 @@ import { connector } from "../types";
 import store from "../store";
 
 export default (connector: connector) => {
-    const account = ref('')
-    const provider = ref({})
-    const error = ref(null)
-    store.currentConnector = connector
-    connector && connector.activate().then(res => {
-        account.value = res.account
-        provider.value = res.provider
-        store.provider = res.provider
-    }).catch(err => {
-        error.value = err
-    })
+    if (store.vueVersion > 3) {
+        const account = ref('')
+        const provider = ref({})
+        const error = ref(null)
+        store.currentConnector = connector
+        connector.activate().then(res => {
+            account.value = res.account
+            provider.value = res.provider
+            store.provider = res.provider
+        }).catch(err => {
+            error.value = err
+        })
 
-    return {
-        account,
-        provider,
-        error
+        return {
+            account,
+            provider,
+            error
+        }
+    } else {
+        return connector.activate()
     }
 }
--- packages/core/src/hooks/useProvider.ts
+++ packages/core/src/hooks/useProvider.ts
@@ -1,9 +1,13 @@
 import { ref } from "vue"
 import store from "../store"
 export default () => {
-    const { provider } = ref({})
-    provider.value = store.provider
-    return {
-        provider
+    if (store.vueVersion > 2) {
+        const { provider } = ref({})
+        provider.value = store.provider
+        return {
+            provider
+        }
+    } else {
+        return store.provider
     }
 }
